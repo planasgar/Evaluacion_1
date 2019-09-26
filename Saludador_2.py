940353d22d889d6840d2e6d2eccd102f4d9600be
Planas <55531110+planasgar@users.noreply.github.com>
2019-09-26 09:42:28 +0200

    Add files via upload

---
+++ Saludador_2.py
@@ -0,0 +1,6 @@
+def saludador_2 ():
+    print "Hola entrenador pokemon, hay muchas variedades de pokemon pero antes de adentrarte a este mundo una pregunta"  #Instruccion de salida
+    Nombre= raw_input("Como te llamas?") #Instruccion de entrada
+    print "Bienvenido al mundo pokemon" + Nombre
+
+saludador_2 () 
