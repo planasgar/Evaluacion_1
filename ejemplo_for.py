179f3e9b1487e72510bccefde9a8dd993428c1ff
Planas <55531110+planasgar@users.noreply.github.com>
2019-10-15 11:13:30 +0200

    Add files via upload

---
+++ ejemplo_for.py
@@ -0,0 +1,8 @@
+def ejemplo_for():
+    #i es una variable que toma los valores comprendidos en el rango
+    final=input("Hasta que numero quieres que cuente: ")
+    for i in range (1,final):
+        print "i= ",i
+
+ejemplo_for()
+    
---
+++ ejemplo_for2.py
@@ -0,0 +1,9 @@
+def ejemplo_for2():
+    #i es una varable que toma los valores comprendidos en el rango
+    inicio=input("Por que numero empiezo: ")
+    final=input("Hasta que numero quieres que cuente: ")
+    for i in range (incio,final):
+        print "i= ",i
+
+ejemplo_for2()
+    
