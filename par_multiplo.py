2298325d8015744c403b1ddff1ee06d50b15b8ab
Planas <55531110+planasgar@users.noreply.github.com>
2019-10-15 11:46:43 +0200

    Add files via upload

---
+++ par_multiplo.py
@@ -0,0 +1,15 @@
+def par_multiplo():
+    print "Dime un numero"
+    a=input("a= ")
+    
+    if (a%2==0):
+        print "Este numero es par"
+    else:
+        print "Este numero es impar"
+
+    if (a%3==0):
+        print "Es multiplo de 3"
+    else:
+        print "No es multiplo de 3"
+
+par_multiplo()
