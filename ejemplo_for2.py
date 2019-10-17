3e8485626e9d40f215fe47951e07466e246b3518
Planas <55531110+planasgar@users.noreply.github.com>
2019-10-17 09:14:37 +0200

    Update ejemplo_for2.py

--- ejemplo_for2.py
+++ ejemplo_for2.py
@@ -2,7 +2,7 @@ def ejemplo_for2():
     #i es una varable que toma los valores comprendidos en el rango
     inicio=input("Por que numero empiezo: ")
     final=input("Hasta que numero quieres que cuente: ")
-    for i in range (incio,final):
+    for i in range (inicio,final):
         print "i= ",i
 
 ejemplo_for2()
